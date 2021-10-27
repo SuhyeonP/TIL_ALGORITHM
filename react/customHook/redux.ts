1473c46a80bc223da4b2ab5e5f9b6c72dc4b8609
suhyeon <ahah12k@gmail.com>
2021-10-27 11:29:13 +0900

    :art: add hook

---
+++ react/customHook/redux.ts
@@ -0,0 +1,10 @@
+import { TypedUseSelectorHook, useDispatch as reduxDispatch, useSelector as reduxSelector } from 'react-redux';
+import type { RootState, AppDispatch } from '../../redux/store';
+
+/**
+ * type 지정 없이 redux Root State 에 접근하는
+ * useSelector, useDispatch
+ */
+export const useSelector: TypedUseSelectorHook<RootState> = reduxSelector;
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const useDispatch = () => reduxDispatch<AppDispatch>();
