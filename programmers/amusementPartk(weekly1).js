a9d5294af9643ee468d26f7081f3c26ead2cd6a6
suhyeonP <ahah12k@gmail.com>
2022-06-11 15:03:08 +0900

    :art: new solution of programmers

--- programmers/amusementPartk(weekly1).js
+++ programmers/amusementPartk(weekly1).js
@@ -1,12 +1,20 @@
+// function solution(price, money, count) {
+//     let need = allAdd(count) * price;
+//
+//     return  money - need >= 0 ? 0 : Math.abs(money  - need);
+// }
+//
+// function allAdd(num) {
+//     return (num + 1) * (num/2)
+// }
+
 function solution(price, money, count) {
-    let need = allAdd(count) * price;
+    const lost = money - (price * (count * (count + 1)) / 2);
 
-    return  money - need >= 0 ? 0 : Math.abs(money  - need);
+    return lost > 0 ? 0 : Math.abs(lost);
 }
 
-function allAdd(num) {
-    return (num + 1) * (num/2)
-}
+console.log(solution(3,20,4))
 
 console.log(solution(3,20,4))
 
