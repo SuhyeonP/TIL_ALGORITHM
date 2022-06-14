7c76cdabe1315e76d96db210dac27b2b1a18aede
suhyeonP <ahah12k@gmail.com>
2022-06-14 21:36:12 +0900

    :bug: fix algorithm

--- ReadyForMove/algorithm/section2/2-1.js
+++ ReadyForMove/algorithm/section2/2-1.js
@@ -1,6 +1,6 @@
 //N(1<=N<=100)개의 정수를 입력받아,
 // 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작 성하세요.(첫 번째 수는 무조건 출력한다)
 
-function solution(base, arr) {
-    return arr.filter((ele) => ele >= base);
+function solution(arr) {
+    return arr.filter((ele, idx) => ele > arr[idx - 1]);
 }
