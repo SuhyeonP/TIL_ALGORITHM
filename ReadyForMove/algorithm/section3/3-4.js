03783082e6fffa671aaaee34f97efe373dd81347
suhyeonP <ahah12k@gmail.com>
2022-06-15 14:50:13 +0900

    :bug: wrong answer

--- ReadyForMove/algorithm/section3/3-4.js
+++ ReadyForMove/algorithm/section3/3-4.js
@@ -5,26 +5,53 @@
 //teachermode e
 //10121012210
 
-function solution(str, s) {
-    const split = str.split(s);
-    const bucket = []
+// 틀림
+// function solution(str, s) {
+//     const split = str.split(s);
+//     const bucket = []
+//
+//     for (let i = 0; i < split.length; i++) {
+//         const part = split[i];
+//         if (part === '') {
+//             bucket.push(0);
+//         } else {
+//             const len = part.length;
+//             for(let j = 0; j < len; j++) {
+//                 const min = Math.min(j + 1, len - j);
+//                 bucket.push(min);
+//             }
+//             bucket.push(0);
+//         }
+//     }
+//
+//     return bucket;
+// }
 
-    for (let i = 0; i < split.length; i++) {
-        const part = split[i];
-        if (part === '') {
-            bucket.push(0);
+function solution2(str, s) {
+    const answer = [];
+    let temp = 0;
+    for (let v of str) {
+        if (v === s) {
+            temp = 0;
+            answer.push(0)
         } else {
-            const len = part.length;
-            for(let j = 0; j < len; j++) {
-                const min = Math.min(j + 1, len - j);
-                bucket.push(min);
-            }
-            bucket.push(0)
+            temp ++;
+            answer.push(temp)
         }
     }
-
-    return bucket;
+    console.log(answer)
+    temp = 0
+    for (let idx in str) {
+        const reverse = str.length - 1 -idx;
+        if(str[reverse] === s) temp = 0;
+        else {
+            temp ++;
+            answer[reverse] = Math.min(answer[reverse], temp);
+        }
+    }
+    return answer;
 }
 
-console.log(solution('teachermode', 'e'))
-console.log(solution('eaeabe', 'e'))
+console.log(solution2('teachermode', 'e'))
+// console.log(solution('teachermode', 'e'))
+console.log(solution2('eaeabe', 'e'))
