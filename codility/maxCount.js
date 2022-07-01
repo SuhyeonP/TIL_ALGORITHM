065e12570c34304f715316e44f8bd8f1126ec709
suhyeonP <ahah12k@gmail.com>
2022-07-01 10:43:56 +0900

    :art: add algorithm

--- codility/maxCount.js
+++ codility/maxCount.js
@@ -35,3 +35,26 @@ function solution(N, A) {
 }
 
 console.log(solution(5,[3,4,4,6,1,4,4]))
+function solution2(N, A) {
+    const answer = Array.from({length: N}, () => 0);
+    let last = 0;
+    let max = 0;
+
+    A.forEach((ele) => {
+        if(ele <= N) {
+            if (answer[ele - 1] < last) {
+                answer[ele - 1] = last;
+            }
+            answer[ele - 1] ++;
+            if (max < answer[ele - 1]) {
+                max = answer[ele - 1];
+            }
+        } else {
+            last = max;
+        }
+    })
+
+    return answer.map((ele) => ele < last ? last : ele);
+}
+
+console.log(solution2(5, [3, 4, 4, 6, 1, 4, 4]))
--- codility/missingInteger.js
+++ codility/missingInteger.js
@@ -18,3 +18,18 @@ console.log(solution([-1, -3]))
 console.log(solution([1,3,6,4,1,2]))
 console.log(solution([2]))
 
+function solution2(arr) {
+    let min = 1;
+
+    arr.sort((a, b) => a - b);
+    arr.forEach((ele) => {
+        if(ele > 0 && ele === min) {
+            min++
+        }
+    });
+
+    return min;
+}
+
+console.log(solution2([1,3,6,4,1,2]))
+console.log(solution2([1, 2, 3]))
